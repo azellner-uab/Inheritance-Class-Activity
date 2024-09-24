a686317ef66644ba5d489070fdeb5734b2b7c94b
Colton Criswell <ctcriswe@uab.edu>
2024-09-24 10:26:50 -0500

    added main

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//main.cpp
+//Inheritance class activity
+
+#include <iostream>
+
+
+
+
+
+
+
+int main(){
+	
+	int choice=0;
+	std::cout << "Welcome to the shelter!" << std::endl;
+	std::cout << "1) Add a dog or cat" << std::endl;
+	std::cout << "2) Remove animal by name" << std::endl;
+	std::cout << "3) Display all animals" << std::endl;
+	std::cout << "4) Exit program" << std::endl;
+	std::cout << "Enter your choice (1-4): ";
+	std::cin >> choice;
+	std::cout << std::endl;
+	while (choice<1 || choice>4){	
+	switch(choice){
+		case 1: 
+			
+		case 2:
+
+		case 3:
+
+		case 4:
+
+		default:	
+	}
+
+	}
+
+
+	return 0;
+}
