cef8d6c09350cd77fb48b294d180559df4f9b7de
Colton Criswell <ctcriswe@uab.edu>
2024-09-24 10:34:50 -0500

    menu loop basically done

--- main.cpp
+++ main.cpp
@@ -2,12 +2,8 @@
 //Inheritance class activity
 
 #include <iostream>
-
-
-
-
-
-
+#include "shelter.h"
+#include "animal.h"
 
 int main(){
 	
@@ -23,18 +19,26 @@ int main(){
 	while (choice<1 || choice>4){	
 	switch(choice){
 		case 1: 
-			
+			addAnimal(Animal* animal);
+			break;	
 		case 2:
-
+			removeAnimal(std::string name);
 		case 3:
-
+			displayAnimals();
 		case 4:
-
-		default:	
+			break;
+		default: 	
+			std::cout << "Please enter a valid input (1-4)" << std::endl;
 	}
-
+	std::cout << "Welcome to the shelter!" << std::endl;
+	std::cout << "1) Add a dog or cat" << std::endl;
+	std::cout << "2) Remove animal by name" << std::endl;
+	std::cout << "3) Display all animals" << std::endl;
+	std::cout << "4) Exit program" << std::endl;
+	std::cout << "Enter your choice (1-4): ";
+	std::cin >> choice;
+	std::cout << std::endl;
 	}
-
 
 	return 0;
 }
