7a4cf5cd0dac0089a26bb23b652c9fe1860887e3
Nandan Patil <nandan311231@gmail.com>
2024-09-26 10:37:55 -0500

    Added comments in animal.cpp

--- animal.cpp
+++ animal.cpp
@@ -4,12 +4,26 @@
 Animal::Animal(const std::string& name, int age, const std::string& type)
     : name(name), age(age), type(type) {}
 
+/* --------------------------------------------------------
+ * Function: displayInfo
+ * Usage: displayInfo()
+ * --------------------------------------------------------
+ * This function displays animal details like name, age and type. 
+ * ---------------------------------------------------------
+ */
 void Animal::displayInfo() const {
     std::cout << "Name: " << name << "\n"
               << "Age: " << age << "\n"
               << "Type: " << type << std::endl;
 }
 
+/* --------------------------------------------------------
+ * Function: getType
+ * Usage: getType()
+ * --------------------------------------------------------
+ * This function returns the type of animal. 
+ * ---------------------------------------------------------
+ */
 std::string Animal::getType() const {
     return type;
 }
@@ -18,6 +32,13 @@ std::string Animal::getType() const {
 Cat::Cat(const std::string& name, int age, const std::string& breed)
     : Animal(name, age, "Cat"), breed(breed) {}
 
+/* --------------------------------------------------------
+ * Function: displayInfo
+ * Usage: displayInfo()
+ * --------------------------------------------------------
+ * This function is called from Cat class object, it override Base class display function.
+ * ---------------------------------------------------------
+ */
 void Cat::displayInfo() const {
     Animal::displayInfo(); // Call base class method
     std::cout << "Breed: " << breed << std::endl;
@@ -27,7 +48,15 @@ void Cat::displayInfo() const {
 Dog::Dog(const std::string& name, int age, const std::string& breed)
     : Animal(name, age, "Dog"), breed(breed) {}
 
+/* --------------------------------------------------------
+ * Function: displayInfo
+ * Usage: displayInfo()
+ * --------------------------------------------------------
+ * This function is called from Dog class object, it override Base class display function.
+ * ---------------------------------------------------------
+ */
 void Dog::displayInfo() const {
     Animal::displayInfo(); // Call base class method
     std::cout << "Breed: " << breed << std::endl;
 }
+
